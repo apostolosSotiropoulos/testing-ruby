f2094584b029e473d4896289094ce67ccda501cb
Apostolos Sotiropoulos <apostolis@theartwishlist.com>
2018-05-24 18:55:08 +0200

    test require refactored to begin rescue

--- test_require.rb
+++ test_require.rb
@@ -2,9 +2,12 @@
 
 require "#{Dir.pwd}/module_container"
 
+puts 'by just using REQUIRE, class methods can be accessed namespaced i.e:'
 Modular::class_printer
 Modular.class_printer
 
-Modular.instance_printer
-
-printer
+begin
+  Modular.instance_printer
+rescue NoMethodError
+  puts 'instance methods are NOT accessible namespaced.'
+end
