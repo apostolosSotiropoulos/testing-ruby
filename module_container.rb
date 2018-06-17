6ce58e72de80efc16d1b22667b3985a3201ac2f5
Apostolos Sotiropoulos <apostolis@theartwishlist.com>
2018-06-17 10:29:06 +0200

    rails is auto including module to call class method

--- module_container.rb
+++ module_container.rb
@@ -1,5 +1,6 @@
 puts 'hello, from module container'
 
+# in rails no include / extend / require is required to call Modular.instance_printer
 module Modular
   def instance_printer
     puts '- instance printer hello'
