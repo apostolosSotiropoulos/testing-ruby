3cd6606f028c6b4cf00daf17abd14cacc9766fb6
Apostolos Sotiropoulos <apostolis@theartwishlist.com>
2018-06-23 12:50:10 +0300

    rails instance and class method in module

--- test_both_instance_and_class_methods_in_module.rb
+++ test_both_instance_and_class_methods_in_module.rb
@@ -21,3 +21,25 @@ end
 puts "class methods say #{Foo.hallo}"
 puts "instance methods say #{Foo.new.hi}"
 puts "instance can also call class method and say #{Foo.new.class.hallo}"
+
+## Rails way: 
+# module MyModule
+#   extend ActiveSupport::Concern
+
+#   def test_instance_method
+#     'instance_method'
+#   end
+
+#   class_methods do
+#     def test_class_method
+#       'class_method'
+#     end
+#   end
+# end
+
+# class Foo
+#   include MyModule
+# end
+
+# puts "this is a #{Foo.test_class_method}"
+# puts "this is a #{Foo.new.test_instance_method}"
