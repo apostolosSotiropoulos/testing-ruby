1fe6075c1c2c3ba148fd7747b0b4df2b4a473bd3
me <me@example.com>
2020-06-06 18:03:45 +0200

    multiples until in arithmetic utils

--- arithmetic_utils.rb
+++ arithmetic_utils.rb
@@ -1,7 +1,24 @@
-# used like: Integer.include(ArithmeticUtils)
+# used like irb "arithmetic_utils.rb"
+
 module ArithmeticUtils
   def digital_root
     return self if self < 10
+
     digits.sum.digital_root
   end
+
+  def multiples_until(limit)
+    [].tap do |m|
+      i = 1
+      while i * self < limit
+        m << i * self
+        i += 1
+      end
+    end
+  end
 end
+
+Integer.include(ArithmeticUtils)
+
+3.multiples_until(1)
+3.multiples_until(10)
